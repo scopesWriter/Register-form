e7f71faa584b1457fedea72b9f92cafb09421d6f
Bishoy Badie <bishoy.badea@evapharma.com>
2022-09-27 14:01:57 +0200

    Initial Commit

---
+++ SwiftUI Arena/SwiftUI_ArenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ArenaApp.swift
+//  SwiftUI Arena
+//
+//  Created by Bishoy Badea [Pharma] on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ArenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
